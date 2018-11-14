f8eb052656770322fe86c2ec840e51c5bf86387d
mlakshmidurga <lakshmidurga.mm@gmail.com>
2018-11-14 12:38:14 +0530

    updated code

---
+++ 6-11-18/project/assets/js/custom.js
@@ -0,0 +1,21 @@
+(function($) {
+ 
+// Preloader 
+    jQuery(window).on('load', function() {
+        jQuery("#status").fadeOut();
+        jQuery("#preloader").delay(200).fadeOut("slow");
+    });
+
+
+    // on ready function
+    $(document).ready(function() {
+		"use strict";
+	
+
+
+});
+		
+
+
+
+})(jQuery);
